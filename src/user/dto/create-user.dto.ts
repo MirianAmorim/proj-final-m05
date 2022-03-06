ac91cabf40cf4de192122238cded96ee04fec3df
Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>
2022-03-06 14:42:45 -0300

    user dto
    Signed-off-by: Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsDate, IsEmail, IsInt, IsNotEmpty, IsString, IsUrl } from 'class-validator';
+
+const moment = require("moment");
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example: 'Mirian Amorim',
+    description: `O nome será utilizado para identificar o usuário.` ,
+  })
+  nome: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  senha: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,79 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { LoginDto } from './../auth/dto/login.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { User, Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtPayload } from 'src/auth/jwt.strategy';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> { //criando user com o prisma
+    data.senha = await bcrypt.hash(data.senha,10)
+    try {
+      const createdUser = await this.prisma.user.create({data});
+      createdUser.senha = undefined;
+      return createdUser;
+    } catch (error) {
+      console.log(error)
+      throw new HttpException('Verifique os dados e tente novamente.', HttpStatus.BAD_REQUEST);
+    }
+    
+  //pegando a senha e passando pelo bcrypt embaralhando 10x  
+    // return await this.prisma.user.create({ data });
+  }
+          //se der erro voltar para CreateUserDto
+  async findByLogin(login: LoginDto): Promise<User>{ //criando login
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: login.email,
+      },
+    }); //testando o dado com o primeiro item encontrado
+    
+    const senhaIgual = await bcrypt.compare(login.senha,user.senha); //comparando a senha inserida com a senha no banco criptografada com bcryot
+    
+    if(!user) {
+      throw new HttpException('Usuário não encontrado', HttpStatus.NOT_FOUND,)
+    }//parando o login quando nao encontrado o user
+
+    if(!senhaIgual) {
+      throw new HttpException('Senha inválida.', HttpStatus.UNAUTHORIZED,)
+    } //parando o login quando senha nao confere
+
+    return user; //retornando user quando validado
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async validateUser(payload: JwtPayload): Promise<User> { //validacao do user pelo payload
+    const user = await this.prisma.user.findFirst({
+      where: 
+      { email: payload.email,
+      }
+    })
+
+    if(!user) {
+      throw new HttpException('Token inválido.', HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findAll():Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: number): Promise<User> {
+    return await this.prisma.user.findUnique({where: {id}});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: number, data: UpdateUserDto): Promise<User> {
+    data.senha = await bcrypt.hash(data.senha,10)
+    return await this.prisma.user.update({data, where: {id}});
+  }
+
+  async remove(id: number): Promise<User> {
+    return await this.prisma.user.delete({where: {id}});
   }
 }
