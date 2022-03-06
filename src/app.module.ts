5c89697cf5fe2e951d2ddc60b8708e40ffd2575a
Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>
2022-03-06 13:50:12 -0300

    Middlewares JWT
    Signed-off-by: Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoriaModule } from './categoria/categoria.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, CategoriaModule],
+  imports: [UserModule, CategoriaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { UserModule } from './../user/user.module';
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { UserService } from 'src/user/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.register({
+      secret: process.env.SECRETKEY, //pegando a senha pra criar token
+      signOptions: {
+        expiresIn: '300s', //tempo de validade do token
+      },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, UserService, PrismaService],
+  controllers: [AuthController],
+  exports: [PassportModule, JwtModule],
+})
+export class AuthModule {}
